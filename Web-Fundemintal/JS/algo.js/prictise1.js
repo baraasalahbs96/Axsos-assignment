8f9b1af1733657b15aeb7dda71f1d27bcb7a2b34
Baraa Salah <baraasalahbs96@gmail.com>
2026-03-23 13:59:03 -0400

    first commit

---
+++ Web-Fundemintal/JS/algo.js/prictise1.js
@@ -0,0 +1,29 @@
+// Declare a variable favoriteAnimal and assign it your favorite animal. Print the value.
+var favoriteAnimal= "cat" 
+console.log(favoriteAnimal);
+// Declare two variables a and b, assign them numbers, and print their sum.
+var a= 3;
+var b= 3;
+console.log(a+b);
+// Create a variable age and assign your age. Print a message like: "I am 25 years old."
+var age= 30;
+var isAge= "I am 30 years old.";
+console.log(isAge);
+// Declare a variable country with a value of your favorite country. Change its value to another country and print it.
+var country= "Maldives";
+var country= "Hawaii";
+console.log(country);
+// Use const to declare a variable for the value of pi (3.14) and print it.
+const pi=3.14;
+console.log(pi);
+// Create a variable named favoriteColor and assign your favorite color to it. Print the value.
+var favoriteColor= "blue";
+console.log(favoriteColor);
+// Create a variable named x with the value 5 and another variable y with the value 10. Print their sum.
+var x= 5;
+var y= 10;
+console.log(x+y);
+// Declare a variable isSunny and set it to true. Print a message like: "It is sunny today."
+var isSunny= true
+var isSunny= "It is sunny today";
+console.log(isSunny);
