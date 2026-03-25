c9aa6a33f02e247fac42d313743295a37b0dc745
Baraa Salah <baraasalahbs96@gmail.com>
2026-03-25 13:36:02 -0400

    first commit

---
+++ Web-Fundemintal/JS/algo.js/practise3.js
@@ -0,0 +1,23 @@
+// Basic Function: Write a function called welcome that prints "Welcome to coding!" when called.
+function welcome(){
+    console.log("welcome to coding!");
+}
+welcome();    
+// With Arguments: Write a function called square that takes a number as input and prints its square.
+function square(num){
+    console.log(num*num);
+}
+// With Return Value: Write a function called convertToCelsiusthat takes a temperature in Fahrenheit and returns the temperature in Celsius.
+
+// Formula: C = (F - 32) * 5 / 9
+function convertToCelsiusthat(F){
+    return C => (F - 32) * 5 / 9;
+}
+let c= convertToCelsiustaht(90);
+console.log(c);
+// Reusable Function: Write a function called isEven that checks if a number is even or odd and returns true or false.
+function isEven(num){
+    return num%2===0;
+}
+console.log(isEven(6));
+console.log(isEven(3));
